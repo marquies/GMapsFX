e676d7657d4ddb27184ee7ac59eb5666ceeec534
Patrick Breucking <me@patrickbreucking.de>
2016-01-17 00:40:07 +0100

    Added methods set/getDraggable

--- GMapsFX/src/main/java/com/lynden/gmapsfx/javascript/object/Marker.java
+++ GMapsFX/src/main/java/com/lynden/gmapsfx/javascript/object/Marker.java
@@ -25,62 +25,73 @@ import com.lynden.gmapsfx.javascript.JavascriptObject;
  */
 public class Marker extends JavascriptObject {
 
+	protected String title;
 
-    protected String title;
-    
-    
-    /**
-     * Contructs a new map Marker with the specified options
-     * @param markerOptions The options to use when constructing this marker.
-     */
-    public Marker(MarkerOptions markerOptions) {
-        super(GMapObjectType.MARKER, markerOptions);
-    }
-    
-    
-    /**
-     * Sets the title of this Marker
-     * @param title The Marker's new title
-     */
-    public void setTitle( String title ) {
-        invokeJavascript("setTitle", title);
-        this.title = title;
-    }
-    
-    
-    /**
-     * This method is called from the GoogleMap.addMarker() method, it should not be invoked directly.
-     *
-     * @param map The map to add this Marker to.
-     */
-    protected void setMap( GoogleMap map ) {
-        invokeJavascript("setMap", map);
-    }
-    
-    
-    /**
-     * Sets how the marker should be animated.  To clear the animation use Animation.NULL
-     * @param animation The animation to use for this marker.
-     */
-    public void setAnimation( Animation animation ) {
-        invokeJavascript("setAnimation", animation);
-    }
-    
-    
-    public void setPosition( LatLong latLong ) {
-        invokeJavascript( "setPosition", latLong );
-    }
+	/**
+	 * Contructs a new map Marker with the specified options
+	 * 
+	 * @param markerOptions
+	 *            The options to use when constructing this marker.
+	 */
+	public Marker(MarkerOptions markerOptions) {
+		super(GMapObjectType.MARKER, markerOptions);
+	}
+
+	/**
+	 * Sets the title of this Marker
+	 * 
+	 * @param title
+	 *            The Marker's new title
+	 */
+	public void setTitle(String title) {
+		invokeJavascript("setTitle", title);
+		this.title = title;
+	}
+
+	/**
+	 * This method is called from the GoogleMap.addMarker() method, it should
+	 * not be invoked directly.
+	 *
+	 * @param map
+	 *            The map to add this Marker to.
+	 */
+	protected void setMap(GoogleMap map) {
+		invokeJavascript("setMap", map);
+	}
+
+	/**
+	 * Sets how the marker should be animated. To clear the animation use
+	 * Animation.NULL
+	 * 
+	 * @param animation
+	 *            The animation to use for this marker.
+	 */
+	public void setAnimation(Animation animation) {
+		invokeJavascript("setAnimation", animation);
+	}
+
+	public void setPosition(LatLong latLong) {
+		invokeJavascript("setPosition", latLong);
+	}
 
 	public void setOptions(MarkerOptions markerOptions2) {
 		invokeJavascript("setOptions", markerOptions2);
 	}
-    
+
 	public void setVisible(boolean visible) {
 		invokeJavascript("setVisible", visible);
 	}
-	
+
 	public boolean getVisible() {
-		return invokeJavascriptReturnValue("getVisible", Boolean.class );
+		return invokeJavascriptReturnValue("getVisible", Boolean.class);
 	}
-    
+
+	public boolean getDraggable() {
+		return invokeJavascriptReturnValue("getDraggable", Boolean.class);
+	}
+
+	public void setDraggable(boolean draggable) {
+		invokeJavascript("setDraggable", draggable);
+	}
+
 }
